3fe2a422828f7e11ec4bc1a4d23fea8e629da2e2
kodjo2022 <107310312+kodjo2022@users.noreply.github.com>
2025-02-03 01:07:44 +0000

    Kodjo-Lab2

--- FutureTime.py
+++ FutureTime.py
@@ -16,13 +16,19 @@ def main():
 
   #TODO:
   #Ask user for hours
+  more_Hours = int(input("Enter more hours : "))
   #Ask user for minutes
-
+  more_Minute = int(input("Enter more minutes : "))
   #Calculate the time after the user-supplied time has passed.
-
+  Total_minutes = (currentMinute + more_Minute)
+  Extra_Hours = Total_minutes // 60
+  future_minutes = Total_minutes % 60
+  Total_Hours = currentHour + more_Hours + Extra_Hours
+  future_hours = Total_Hours % 24
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
+  print ( future_hours, future_minutes)
 
 
 if __name__ == '__main__':
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,9 +10,20 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
+  answers = [ 
+    "As I see it, yes.", "Cannot predict now.", "Concentrate and ask again.", "Most likely.", "Ask again later.", "Don't rely on it.", "Interesting!."
+    "Ask again later.",  "Don’t count on it.", "My reply is no.", "Reply hazy, try again.", "My sources say no.", "Yes,understood your consern.",
+    "Better not tell you now.", "It is certain.", "It is decidedly so.", "Outlook not so good.", "It is certain.", "Amazing, let try it!", 
+    "Signs point to yes.", "Very doubtful.", "Without a doubt.", "You may rely on it. ", "Yes – definitely."
+  ]
 
   #Answer question randomly with one of the options from your earlier list.
-
+  length = len(answers)
+  r = random.random() * length
+  r = int (r)
+  print (r)
+  response = answers [r]
+  print (response)
 
 if __name__ == '__main__':
   main()
